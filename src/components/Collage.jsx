1e074b55d9808df3c29bc2c5128b620658a1c19d
Diego Diaz De La Concha <diegodiazdelaconcha93@gmail.com>
2024-10-03 11:18:45 -0600

    desarrollo del collage y cada IconsTecnologie

--- src/components/Collage.jsx
+++ src/components/Collage.jsx
@@ -6,50 +6,50 @@ export default function Collage(props) {
     <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle}</p>
-        <h4 className="text-white font-medium text-large">{props.title}</h4>
+        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle}</p>
+        <h4 className="text-black font-medium text-large">{props.title}</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-4.jpeg"
+        src={props.img1}
       />
     </Card>
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle2}</p>
-        <h4 className="text-white font-medium text-large">{props.title2}</h4>
+        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle2}</p>
+        <h4 className="text-black font-medium text-large">{props.title2}</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-3.jpeg"
+        src={props.img2}
       />
     </Card>
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle3}</p>
-        <h4 className="text-white font-medium text-large">{props.title3}</h4>
+        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle3}</p>
+        <h4 className="text-black font-medium text-large">{props.title3}</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-2.jpeg"
+        src={props.img3}
       />
     </Card>
     <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
       <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle4}</p>
+        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle4}</p>
         <h4 className="text-black font-medium text-2xl">{props.title4}</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card example background"
         className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-        src="https://nextui.org/images/card-example-6.jpeg"
+        src={props.img4}
       />
       <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
         <div>
@@ -70,7 +70,7 @@ export default function Collage(props) {
         removeWrapper
         alt="Relaxing app background"
         className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-5.jpeg"
+        src={props.img5}
       />
       <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
         <div className="flex flex-grow gap-2 items-center">
