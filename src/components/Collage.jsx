3433403263b26066cf9af8fbba415e32c8860768
Diego Diaz De La Concha <diegodiazdelaconcha93@gmail.com>
2024-10-04 20:20:05 -0600

    80% completado los collage, solo falta los titulos

--- src/components/Collage.jsx
+++ src/components/Collage.jsx
@@ -1,92 +1,69 @@
-import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
 
 export default function Collage(props) {
 
   return (
     <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle}</p>
-        <h4 className="text-black font-medium text-large">{props.title}</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src={props.img1}
-      />
-    </Card>
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle2}</p>
-        <h4 className="text-black font-medium text-large">{props.title2}</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src={props.img2}
-      />
-    </Card>
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle3}</p>
-        <h4 className="text-black font-medium text-large">{props.title3}</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src={props.img3}
-      />
-    </Card>
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-black/60 uppercase font-bold">{props.subtitle4}</p>
-        <h4 className="text-black font-medium text-2xl">{props.title4}</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card example background"
-        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-        src={props.img4}
-      />
-      <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-        <div>
-          <p className="text-black text-tiny">Available soon.</p>
-          <p className="text-black text-tiny">Get notified.</p>
-        </div>
-        <Button className="text-tiny" color="primary" radius="full" size="sm">
-          Notify Me
-        </Button>
-      </CardFooter>
-    </Card>
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle5}</p>
-        <h4 className="text-white/90 font-medium text-xl">{props.title5}</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Relaxing app background"
-        className="z-0 w-full h-full object-cover"
-        src={props.img5}
-      />
-      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-        <div className="flex flex-grow gap-2 items-center">
-          <Image
-            alt="Breathing app icon"
-            className="rounded-full w-10 h-11 bg-black"
-            src="https://nextui.org/images/breathing-app-icon.jpeg"
-          />
-          <div className="flex flex-col">
-            <p className="text-tiny text-white/60">Breathing App</p>
-            <p className="text-tiny text-white/60">Get a good nights sleep.</p>
-          </div>
-        </div>
-        <Button radius="full" size="sm">Get App</Button>
-      </CardFooter>
-    </Card>
-  </div>
+      <Card className="col-span-12 sm:col-span-4 p-2">
+        <CardHeader className="flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle}</p>
+          <h4 className="text-white font-medium text-large">{props.title}</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="w-48 h-auto mx-auto"
+          src={props.img1}
+        />
+      </Card>
+      <Card className="col-span-12 sm:col-span-4 p-2">
+        <CardHeader className="flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle2}</p>
+          <h4 className="text-white font-medium text-large">{props.title2}</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="w-48 h-auto mx-auto"
+          src={props.img2}
+        />
+      </Card>
+      <Card className="col-span-12 sm:col-span-4 p-2">
+        <CardHeader className="flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle3}</p>
+          <h4 className="text-white font-medium text-large">{props.title3}</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="w-48 h-auto mx-auto"
+          src={props.img3}
+        />
+      </Card>
+      <Card  className="w-full h-[300px] col-span-12 sm:col-span-5">
+        <CardHeader className="flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle4}</p>
+          <h4 className="text-white font-medium text-2xl">{props.title4}</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card example background"
+          className="w-full h-auto my-auto"
+          src={props.img4}
+        />
+      </Card>
+      <Card  className="w-full h-[300px] col-span-12 sm:col-span-7">
+        <CardHeader className="flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">{props.subtitle5}</p>
+          <h4 className="text-white/90 font-medium text-xl">{props.title5}</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Relaxing app background"
+          className="w-full h-auto"
+          src={props.img5}
+        />
+      </Card>
+    </div>
   );
 }
